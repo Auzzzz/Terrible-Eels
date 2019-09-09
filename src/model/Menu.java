73638257edc12c3f969db5ad04eb318cc4613e37
Midori Verdouw <s3575912@student.rmit.edu.au>
2019-09-09 13:27:47 +1000

    Made some change on loop and handling options

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -28,7 +28,7 @@ public class Menu {
 			System.out.println("Please Enter Password:");
 			input = scanner.nextLine();
 
-			while (input != "1") {
+			while (true) {
 				System.out.println("Press 1 to Display All Projects ");
 				System.out.println("Press 2 to Display Popular Projects ");
 				System.out.println("Press 3 to exit");
@@ -37,21 +37,28 @@ public class Menu {
 
 				switch (input) {
 				case "1":
+					System.out.println("All Projects:");
 					System.out.println(system.getAllProjectDescs());
 					break;
 				case "2":
+					System.out.println("Popular Projects:");
 					System.out.println(system.getPopularProjectDescs());
+					break;
+				case "3":
+					System.out.println("Exited");
 					break;
 				default:
 					System.out.println("Wrong button pressed, please try again.");
 					break;
 
 				}
-
-				scanner.close();
-				break;
+				System.out.println("\n");
+				
+				if (input.equals("3")) {
+					scanner.close();
+					break;
+				}
 			}
 		}
-
 	}
 }
