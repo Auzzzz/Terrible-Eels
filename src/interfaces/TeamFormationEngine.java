480acfe7dbb94d9ff509db0f72d40489e1d74456
Midori Verdouw <s3575912@student.rmit.edu.au>
2019-09-30 22:35:26 +1000

    Refactored System by splitting out new Engine class

--- src/interfaces/TeamFormationEngine.java
+++ src/interfaces/TeamFormationEngine.java
@@ -2,16 +2,20 @@ package interfaces;
 
 import java.util.Collection;
 
-import model.RoleRequirement;
 import model.teamFormation.InsufficientProjectsException;
 import model.teamFormation.InsufficientStudentsException;
 import model.teamFormation.RemainedStudentsException;
 
-public interface ProjectTeamsFormationSystem {
-	Collection<String> getAllProjectDescs();
+public interface TeamFormationEngine {
 	
-	Collection<String> getPopularProjectDescs();
-	
+	/**
+	 * get popular projects as a list of Projects
+	 * the number of popular projects equals to the number of teams to be formed
+	 * @return - list of Projects
+	 * @throws InsufficientStudentsException 
+	 */
+	Collection<Project> getPopularProjects() throws InsufficientStudentsException;
+		
 	/**
 	 * swaps members between teams as long as the overall fitness value for swapped teams 
 	 * does not change by more than the specified value
@@ -22,6 +26,7 @@ public interface ProjectTeamsFormationSystem {
 	 */
 	boolean swap(Student s1, Student s2, int acceptableChange);
 	
+	
 	/**
 	 * assign all students into projects
 	 * @return - true if all students are successfully assigned to projects
@@ -31,33 +36,4 @@ public interface ProjectTeamsFormationSystem {
 	 */
 	boolean assignStudents() throws InsufficientProjectsException, InsufficientStudentsException, RemainedStudentsException;
 
-	/**
-	 * adds a project to the database
-	 * @param desc
-	 * @param roles
-	 */
-	void addProject(String desc, Collection<RoleRequirement> roles);
-	
-	/**
-	 * sets the preferences of a student
-	 * @param student
-	 * @param projects
-	 */
-	void setPreferences(Student student, Collection<Project> projects);
-	
-	/**
-	 * sets the blacklist of a student
-	 * @param student
-	 * @param students
-	 */
-	void setBlacklist(Student student, Collection<Student> students);
-
-	/**
-	 * sets the roles of a student
-	 * @param student
-	 * @param roles
-	 */
-	void setRoles(Student student, Collection<RoleRequirement> roles);
-	
-	
 }
