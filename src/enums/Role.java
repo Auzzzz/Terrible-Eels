e39dd75eccc5038977c945173fba8974f0a25903
Midori Verdouw <s3575912@student.rmit.edu.au>
2019-10-10 23:27:31 +1100

    Added constructor in enums

--- src/enums/Role.java
+++ src/enums/Role.java
@@ -1,8 +1,18 @@
 package enums;
 
 public enum Role {
-	LEADER,
-	DBA,
-	PROGRAMMER,
-	UIDESIGNER
+	LEADER(0),
+	DBA(1),
+	PROGRAMMER(2),
+	UIDESIGNER(3);
+	
+	private final int id;
+	
+	Role(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
 }
--- src/enums/Skill.java
+++ src/enums/Skill.java
@@ -1,7 +1,17 @@
 package enums;
 
 public enum Skill {
-ORACLE,
-JAVA,
-CPLUSPLUS
+	ORACLE(0),
+	JAVA(1),
+	CPLUSPLUS(2);
+	
+	private final int id;
+	
+	Skill(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
 }
